1cdcd4ee878379968c91c89c0a23ed8691804f93
HoeSeong123 <ghltjd3@gmail.com>
2023-04-24 13:35:04 +0900

    세팅

---
+++ src/main/java/com/ll/exam/app20230424/home/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.ll.exam.app20230424.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain() {
+        return "Hello World";
+    }
+}
